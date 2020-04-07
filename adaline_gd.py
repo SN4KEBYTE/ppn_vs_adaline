60c13668cfd08c3b74a47e3ec3747fb8997bbaa2
SN4KEBYTE <timurcat7@gmail.com>
2020-04-08 01:49:27 +0700

    Comments fixes.

--- adaline_gd.py
+++ adaline_gd.py
@@ -4,18 +4,17 @@ from slnn import SLNN
 
 class AdalineGD(SLNN):
     def __init__(self, eta=0.01, n_iter=10):
-        super().__init__(self, eta, n_iter)  # темп обучения, число эпох
+        super().__init__(eta, n_iter)  # темп обучения, число эпох
         self.__w = None  # веса
-        self.__cost = None  # стоимость
+        self.__cost = []  # стоимость
 
     def fit(self, X, y):
         """Выполнить подгонку под тренировочные данные"""
         self.__w = np.zeros(1 + X.shape[1])  # инициализуруем весовой вектор
-        self.__cost = []
 
         for _ in range(self.n_iter):
-            output = self.net_input(X)  # чистый вход
-            errors = y - output  # число случаев ошибочной классификации
+            output = self.__net_input(X)  # чистый вход
+            errors = y - output  # отклонения расчетных результатов от истинных меток классов
             self.__w[1:] += self.eta * X.T.dot(errors)  # обновляем веса, вычислив градиент
             self.__w[0] += self.eta * errors.sum()  # не забываем про вес с нулевым индексом
             cost = (errors ** 2).sum() / 2.0  # считыаем стоимость как SSE
@@ -30,7 +29,7 @@ class AdalineGD(SLNN):
 
     def __activation(self, X):
         """Рассчитать линейную активацию"""
-        return self.net_input(X)
+        return self.__net_input(X)
 
     def predict(self, X):
         """Вернуть метку класса после единичного скачка"""
@@ -40,3 +39,4 @@ class AdalineGD(SLNN):
     def costs(self):
         """Вернуть стоимость в каждой эпохе"""
         return self.__cost
+
